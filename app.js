b4ea25329fdb5cd33fcea5a89dbedb16d408fb8e
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-02 16:25:27 +0100

    feat: exercise completed

--- app.js
+++ app.js
@@ -6,46 +6,46 @@ const recipes = [
     {
         titolo: "Ciambella",
         contenuto: "Torta Savarin",
-        immagine: "",
+        immagine: "ciambellone.jpeg",
         tags: ["pasticceria", "uova", "latte", "zucchero"]        
     },
     {
         titolo: "Cracker",
         contenuto: "Cracker alla barbabietola",
-        immagine: "",
+        immagine: "cracker_barbabietola.jpeg",
         tags: ["croccante", "healthy", "biologico", "tostato"]
     },
     {
         titolo: "Pane fritto",
         contenuto: "Pane dolce fritto",
-        immagine: "",
+        immagine: "pane_fritto_dolce.jpeg",
         tags: ["croccante", "fritto", "farina", "zucchero"]
     },
     {
         titolo: "Pasta",
         contenuto: "Pasta alla barbabietola",
-        immagine: "",
+        immagine: "pasta_barbabietola.jpeg",
         tags: ["sugo", "vegetale", "biologico", "buona"]
     },
     {
         titolo: "Torta",
         contenuto: "Torta paesana",
-        immagine: "",
+        immagine: "torta_paesana.jpeg",
         tags: ["morbida", "pane", "cacao", "pinoli"]
     },
 ];
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.json ({
+    res.json({
         message: "Server del mio blog"
     });
 });
 
 app.get('/bacheca', (req, res) => {
-    res.json ({
-
-    })
-})
+    res.json(recipes);
+});
 
 app.listen(port, () => {
     console.log('server partito');
